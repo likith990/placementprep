67678183f7be10f3777a6de00c7dd6dc0fe0a649
likith <ammangilikith@gmail.com>
2026-07-23 19:41:07 +0530

    Connect MongoClient for Better Auth

--- client/src/lib/auth-client.js
+++ client/src/lib/auth-client.js
@@ -3,4 +3,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: "https://placementprep-3m4y.onrender.com",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
