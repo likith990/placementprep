31a6a4eb86bf9fb4c7532bd6d8de7b01af7cd04f
likith <ammangilikith@gmail.com>
2026-07-29 19:41:48 +0530

    makinng changes to the buttons and also visibility of th elinks

--- client/src/components/ui/CreateButton.jsx
+++ client/src/components/ui/CreateButton.jsx
@@ -5,22 +5,24 @@ export default function CreateButton({openForm}){
     return(
         <>
         <Button
-      onClick={openForm}
-      sx={{
-        textTransform: "none",
-        fontWeight: 600,
-        fontSize: 14,
-        backgroundColor: "#4f46e5",
-        borderRadius: "8px",
-        boxShadow: "none",
-        "&:hover": {
-          backgroundColor: "#4338ca",
-          boxShadow: "none",
-        },
-      }}
-    >
-      Create Slot
-    </Button>
+  variant="contained"
+  onClick={openForm}
+  sx={{
+    textTransform: "none",
+    fontWeight: 600,
+    fontSize: 14,
+    color: "#fff",
+    backgroundColor: "#4f46e5",
+    borderRadius: "8px",
+    boxShadow: "none",
+    "&:hover": {
+      backgroundColor: "#4338ca",
+      boxShadow: "none",
+    },
+  }}
+>
+  Create Slot
+</Button>
         </>
     )
 }
